5571db6c0367f91a842ac0e7719cd7f5382926f9
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-11-16 13:42:14 +0100

    initialized a client

--- context_memory.py
+++ context_memory.py
@@ -1,5 +1,11 @@
 import sys
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def initialize_client(use_ollama: bool= False)->OpenAI:
     """Initialize the OpenAI client for either OpenAi or OLLAMA"""
+    if use_ollama:
+        return OpenAI(base_url="http://localhost:11434/v1",api_key="ollama")
+    return OpenAI()
