ebb003a69299bf6ce7a0d6d70335865076a2c561
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-11-16 15:16:44 +0100

    saved conversation and added memory

--- context_memory.py
+++ context_memory.py
@@ -1,7 +1,9 @@
+import json
 import sys
 from typing import Dict, List
 from openai import OpenAI
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -36,3 +38,64 @@ def summarize_messages(messages: List[Dict[str,str]])->List[Dict[str,str]]:
         m["content"][:50]+ "..." for m in messages[-5:] 
     ])   
     return [{"role":"system", "content": summary}] + messages[-5:]
+
+def save_conversation(messages:List[Dict[str,str]], filename: str = "conversation.json"):
+    """Save conversation to a file"""
+    with open(filename,"w") as f:
+        json.dump(messages,f)
+        
+def load_conversation(filname:str="conversation.json")-> List[Dict[str,str]]:
+    """Loads previous conversations from file"""
+    try:
+        with open(filname, "r") as f:
+            return   json.load(f)
+    except FileNotFoundError:
+        print(f"No Conversation file found at{filname}")
+        return create_initial_messages()
+    
+
+def main():
+    print("Select model type:")
+    print("1. OpenAI GPT 4")
+    print("2. Llama (local)")
+    
+    choice = input("Enter choice(1 or 2): ")
+    use_ollama = choice == '2'
+    
+    client = initialize_client(use_ollama)
+    model_name = "llama3.2" if use_ollama else "gpt-4o-mini"
+    messages= create_initial_messages()
+    
+    print(f"\nUsing {'Ollama' if use_ollama else 'OpenAI'} model.")
+    print("Available commands:")
+    print("-'save': Save conversation")
+    print("-'load': Load conversation")
+    print("-'summary': Summarize conversation")
+    print("-'quit': Quit conversation")
+    
+    
+    while True:
+        user_input = input("\nYou: ")
+        if user_input.lower() =="quit":
+            break
+        elif user_input.lower() =="save":
+            save_conversation(messages)
+            print("Conversation Saved!")
+            continue
+        elif user_input.lower() =="load":
+            messages = load_conversation()
+            print("Conversation loaded")     
+        elif user_input.lower() == "summary":
+            messages = summarize_messages()
+            print("Summarized messages")  
+            continue  
+        
+        response = chat(user_input, messages,client, model_name)
+        print(f"\nAssitant: {response}")
+        
+        if len(messages) > 10:
+            messages = summarize_messages(messages)
+            print("\n(Conversation automatically summarized)")
+            
+if __name__== "__main__": 
+    main()           
